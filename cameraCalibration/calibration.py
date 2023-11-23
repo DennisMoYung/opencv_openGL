99aaf522f9d486857b55150e18aeda5d537a04b8
Wong Wai Chun <kelvinwong159357@gmail.com>
2023-11-24 00:53:32 +0800

    new version

--- cameraCalibration/calibration.py
+++ cameraCalibration/calibration.py
@@ -1,109 +1,44 @@
+import cv2
 import numpy as np
-import cv2 as cv
 import glob
 
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# Define the checkerboard size
+CHECKERBOARD = (6, 8)  # Number of inner corners per a checkerboard row and column
 
-# Define the dimensions of checkerboard 
-CHECKERBOARD = (6, 8) 
+# Arrays to store object points and image points.
+objpoints = []  # 3d points in real world space
+imgpoints = []  # 2d points in image plane.
 
-# Vector for 3D points 
-threedpoints = [] 
-  
-# Vector for 2D points 
-twodpoints = [] 
+# Prepare object points like (0,0,0), (1,0,0), (2,0,0) ..., (5,7,0)
+objp = np.zeros((CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+objp[:, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 
-#  3D points real world coordinates 
-objectp3d = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1],  3), np.float32)
+# Loop over all calibration images
+images = glob.glob('path/to/calibration/images/*.jpg')
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-objectp3d[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
-prev_img_shape = None
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, None)
 
+    # If found, add object points, image points
+    if ret:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+        
+#This step computes the camera matrix, distortion coefficients, rotation and translation vectors etc.
+ret, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-# Extracting path of individual image stored 
-# in a given directory. Since no path is 
-# specified, it will take current directory 
-# jpg files alone 
-images = glob.glob('./images/*.png')
-
-for filename in images: 
-    image = cv.imread(filename) 
-    grayColor = cv.cvtColor(image, cv.COLOR_BGR2GRAY) 
-  
-    # Find the chess board corners 
-    # If desired number of corners are 
-    # found in the image then ret = true 
-    ret, corners = cv.findChessboardCorners( 
-                    grayColor, CHECKERBOARD,  
-                    cv.CALIB_CB_ADAPTIVE_THRESH  
-                    + cv.CALIB_CB_FAST_CHECK + 
-                    cv.CALIB_CB_NORMALIZE_IMAGE) 
-  
-    # If desired number of corners can be detected then, 
-    # refine the pixel coordinates and display 
-    # them on the images of checker board 
-    if ret == True: 
-        threedpoints.append(objectp3d) 
-  
-        # Refining pixel coordinates 
-        # for given 2d points. 
-        corners2 = cv.cornerSubPix( 
-            grayColor, corners, (11, 11), (-1, -1), criteria) 
-  
-        twodpoints.append(corners2) 
-  
-        # Draw and display the corners 
-        image = cv.drawChessboardCorners(image,  
-                                          CHECKERBOARD,  
-                                          corners2, ret) 
-  
-    cv.imshow('img', image) 
-    cv.waitKey(0) 
-  
-cv.destroyAllWindows() 
-  
-h, w = image.shape[:2]
-
-# Perform camera calibration by 
-# passing the value of above found out 3D points (threedpoints) 
-# and its corresponding pixel coordinates of the 
-# detected corners (twodpoints) 
-ret, matrix, distortion, r_vecs, t_vecs = cv.calibrateCamera( 
-    threedpoints, twodpoints, grayColor.shape[::-1], None, None) 
-
-# Displaying required output 
-print(" Camera matrix:") 
-print(matrix) 
-  
-print("\n Distortion coefficient:") 
-print(distortion) 
-  
-print("\n Rotation Vectors:") 
-print(r_vecs) 
-  
-print("\n Translation Vectors:") 
-print(t_vecs)
-
-
-############## UNDISTORTION #####################################################
-
-img = cv.imread('./images/img0.png')
-h,  w = img.shape[:2]
-newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(matrix, distortion, (w,h), 1, (w,h))
-
-# undistort
-dst = cv.undistort(img, matrix, distortion, None, newCameraMatrix)
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('./images/calibresult.png', dst)
-
-# Reprojection Error
+#calculating the reprojection error, which measures how close the projected points are to the actual image points.
 mean_error = 0
-for i in range(len(threedpoints)):
-    imgpoints2, _ = cv.projectPoints(threedpoints[i], r_vecs[i], t_vecs[i], matrix, distortion)
-    error = cv.norm(twodpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, distCoeffs)
+    error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
     mean_error += error
-print( "\ntotal error: {}".format(mean_error/len(threedpoints)) )
+print("Total error: ", mean_error / len(objpoints))
 
+
+# Save the camera matrix and distortion coefficients
+np.save("cameraMatrix.npy", cameraMatrix)
+np.save("distCoeffs.npy", distCoeffs)
